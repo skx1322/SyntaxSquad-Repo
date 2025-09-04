d19af5b4f4be503a9b0931c9d3f1212cbaf0b704
skx1322 <kaixing636@gmail.com>
2025-09-04 18:20:16 +0800

    Added LOGIN/REGISTER component in account.jsx page

--- client/src/pages/account.jsx
+++ client/src/pages/account.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import * as React from 'react'
+import Login from './partial/login'
+import Register from './partial/register'
 
 const Account = () => {
+  const [currentPage, setCurrentPage] = React.useState("register")
+  console.log(currentPage);
   return (
-    <div>Account</div>
+    <main>
+      {
+        currentPage === "login" ? (<Login onClickPage={setCurrentPage}></Login>) : (<Register onClickPage={setCurrentPage}></Register>)
+      }
+    </main>
   )
 }
 
